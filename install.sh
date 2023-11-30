60a201ed4637a95aae961addb24b4996789a39d3
Sarankon Hiranphonkrit <sarankon.hi@gmail.com>
2023-11-30 10:28:51 +0700

    update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,12 @@ openrc boot
 
 apk add subversion
 apk add apache2 apache2-utils apache2-webdav mod_dav_svn
+
+cd /etc/apache2/conf.d/
+wget https://raw.githubusercontent.com/sarankon/alpine-subversion/main/etc/apache2/conf.d/subversion.conf
+
+cd /home
+wget https://raw.githubusercontent.com/sarankon/alpine-subversion/main/utils.sh
+
+mv utils.sh svn-utils.sh
+chmod +x svn-utils.sh
